28ac0251a02a661762e487e38a26f297a9d7400e
harshkumar1302 <bs824536@gmail.com>
2024-05-25 14:17:26 +0530

    Dev - created a page for portfolio-showcase

---
+++ src/app/showcase/page.tsx
@@ -0,0 +1,21 @@
+import PortfolioShowcase from "@/components/Showcase/Portfolio-showcase";
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+
+
+
+export const metadata: Metadata = {
+    title:
+        "Next.js E-commerce Dashboard | TailAdmin - Next.js Dashboard Template",
+    description: "This is Next.js Home for TailAdmin Dashboard Template",
+};
+
+export default function Home() {
+    return (
+        <>
+            <DefaultLayout>
+                <PortfolioShowcase />
+            </DefaultLayout>
+        </>
+    );
+}
